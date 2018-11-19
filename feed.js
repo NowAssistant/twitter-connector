b426e1eff422810e935192a8b16af5332bce9735
olliejm <olliejm@protonmail.com>
2018-11-19 12:00:43 +0000

    implement pagination and daterange, refactor, fix age string bug, use string instead of int ids

--- feed.js
+++ feed.js
@@ -2,18 +2,28 @@ const got = require('got');
 const Autolinker = require('autolinker');
 const moment = require('moment');
 
+let startDate = null;
+let endDate = null;
+
+let action = null;
+let page = null;
+let pageSize = null;
+
 module.exports = async activity => {
     try {
         const token = await access_token();
 
         if (token) {
             const sources = activity.Context.connector.custom3.split(',');
-            const items = [];
+            
+            let items = [];
+
+            configure_range();
 
             for (let i = 0; i < sources.length; i++) {
                 const endpoint = 
                     activity.Context.connector.endpoint + 
-                    '?screen_name=' + sources[i] + '&count=10&tweet_mode=extended';
+                    '?screen_name=' + sources[i];
 
                 const response = await got(endpoint, {
                     headers: {
@@ -30,21 +40,17 @@ module.exports = async activity => {
                 }
             }
 
-            activity.Response.Data.items = items
-                .sort(
-                    function(a, b) {
-                        a = new Date(a.date);
-                        b = new Date(b.date);
+            items = items.sort(dateDescending);
 
-                        return a > b 
-                            ? - 1 
-                            : a < b 
-                                ? 1 
-                                : 0;
-                    })
-                .splice(0, 10);
+            activity.Response.Data.items = [];
 
-            return activity.Response.Data.items;
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+
+                if (!skip(i, items.length, new Date(item.date))) {
+                    activity.Response.Data.items.push(item);
+                }
+            }
         } else {
             activity.Response.ErrorCode = 403;
             activity.Response.Data = {
@@ -66,61 +72,7 @@ module.exports = async activity => {
         };
     }
 
-    function convert_item(_item) {
-        const item = {
-            user: {
-                id: _item.user.id,
-                screen_name: _item.user.screen_name,
-                name: _item.user.name
-            },
-            id: _item.id,
-            text: Autolinker.link(_item.full_text, {
-                hashtag: 'twitter',
-                mention: 'twitter'
-            }),
-            favourites: _item.favorite_count,
-            retweets: _item.retweet_count,
-            date: new Date(_item.created_at).toISOString()
-        };
-
-        item.age = moment(item.date).fromNow(true)
-            .replace('an', '1')
-            .replace('a', '1')
-            .replace(/ (second)(s\b|\b)/, 's')
-            .replace(/ (minute)(s\b|\b)/, 'm')
-            .replace(/ (hour)(s\b|\b)/, 'h')
-            .replace(/ (day)(s\b|\b)/, 'd')
-            .replace(/ (year)(s\b|\b)/, 'y');
-
-        if (_item.extended_entities && _item.extended_entities.media) {
-            item.thumbnail = _item.extended_entities.media[0].media_url_https;
-        }
-
-        if (_item.entities.symbols && _item.entities.symbols.length > 0) {
-            const regex = /\$[A-Za-z]{1,6}([._][A-Za-z]{1,2})?/g;
-            const matches = _item.full_text.match(regex);
-
-            if (matches) {
-                for (let i = 0; i < matches.length; i++) {
-                    const norm = matches[i].replace('$', '%24');
-
-                    item.text = item.text
-                        .replace(
-                            matches[i],
-                            '<a href="https://twitter.com/search?q=' + 
-                            norm + 
-                            '" target="_blank" rel="noopener noreferrer">' + 
-                            matches[i] + 
-                            '</a>');
-                }
-            }
-        }
-
-        item.text = item.text
-            .replace(/<a href/g, '<a class="blue" href');
-
-        return item;
-    }
+    return activity.Response.Data.items; // support cloud connectors
 
     async function access_token() {
         const credentials = new Buffer.from(
@@ -146,13 +98,130 @@ module.exports = async activity => {
 
         return null;
     }
+    
+    function configure_range() {
+        if (activity.Request.Query.startDate) {
+            startDate = convert_date(activity.Request.Query.startDate);
+        }
 
-    function rfc_encode(key) {
-        return encodeURIComponent(key)
-            .replace(/!/g, '%21')
-            .replace(/'/g, '%27')
-            .replace(/\(/g, '%28')
-            .replace(/\)/g, '%29')
-            .replace(/\*/g, '%2A');
+        if (activity.Request.Query.endDate) {
+            endDate = convert_date(activity.Request.Query.endDate);
+        }
+
+        if (activity.Request.Query.page && activity.Request.Query.pageSize) {
+            action = "firstpage"
+            page = parseInt(activity.Request.Query.page);
+            pageSize = parseInt(activity.Request.Query.pageSize);
+
+            if (activity.Request.Data &&
+                activity.Request.Data.args &&
+                activity.Request.Data.args.atAgentAction == "nextpage") {
+                    action = "nextpage";
+                    page = parseInt(activity.Request.Data.args._page) || 2;
+                    pageSize = parseInt(activity.Request.Data.args._pageSize) || 20;
+            }
+        } else if (activity.Request.Query.pageSize) {
+            pageSize = parseInt(acitivty.Request.Query.pageSize);
+        } else {
+            pageSize = 10;
+        }
     }
 };
+
+const dateDescending = (a, b) => {
+    a = new Date(a.date);
+    b = new Date(b.date);
+        
+    return a > b ? -1 : (a < b ? 1 : 0);
+}
+
+function rfc_encode(key) {
+    return encodeURIComponent(key)
+        .replace(/!/g, '%21')
+        .replace(/'/g, '%27')
+        .replace(/\(/g, '%28')
+        .replace(/\)/g, '%29')
+        .replace(/\*/g, '%2A');
+}
+
+function skip(i, length, date) {
+    if (startDate && endDate) {
+        return date < startDate || date > endDate;
+    } else if (startDate) {
+        return date < startDate;
+    } else if (endDate) {
+        return date > endDate;
+    } else if (page && pageSize) {
+        const startItem = Math.max(page - 1, 0) * pageSize;
+        const endItem = startItem + pageSize;
+
+        if (endItem > length) {
+            endItem = length;
+        }
+
+        return i < startItem || i >= endItem;
+    } else if (pageSize) {
+        return i > pageSize - 1;  
+    } else {
+        return false;
+    }
+}
+
+function convert_item(_item) {
+    const item = {
+        user: {
+            id: _item.user.id_str,
+            screen_name: _item.user.screen_name,
+            name: _item.user.name
+        },
+        id: _item.id_str,
+        text: Autolinker.link(_item.full_text, {
+            hashtag: 'twitter',
+            mention: 'twitter'
+        }),
+        favourites: _item.favorite_count,
+        retweets: _item.retweet_count,
+        date: new Date(_item.created_at).toISOString(),
+    };
+
+    item.age = tweet_age(item.date);
+
+    if (_item.extended_entities && _item.extended_entities.media) {
+        item.thumbnail = _item.extended_entities.media[0].media_url_https;
+    }
+
+    if (_item.entities.symbols && _item.entities.symbols.length > 0) {
+        const regex = /\$[A-Za-z]{1,6}([._][A-Za-z]{1,2})?/g;
+        const matches = _item.full_text.match(regex);
+
+        if (matches) {
+            for (let i = 0; i < matches.length; i++) {
+                const enc = matches[i].replace('$', '%24');
+
+                item.text = item.text
+                    .replace(
+                        matches[i],
+                        '<a href="https://twitter.com/search?q=' + 
+                        enc + 
+                        '" target="_blank" rel="noopener noreferrer">' + 
+                        matches[i] + 
+                        '</a>');
+            }
+        }
+    }
+
+    item.text = item.text.replace(/<a href/g, '<a class="blue" href');
+
+    return item;
+}
+
+function tweet_age(date) {
+    return moment(date).fromNow(true)
+        .replace('an ', '1 ')
+        .replace('a ', '1 ')
+        .replace(/ (second)(s\b|\b)/, 's')
+        .replace(/ (minute)(s\b|\b)/, 'm')
+        .replace(/ (hour)(s\b|\b)/, 'h')
+        .replace(/ (day)(s\b|\b)/, 'd')
+        .replace(/ (year)(s\b|\b)/, 'y');
+}
